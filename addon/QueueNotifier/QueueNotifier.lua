9fd546cc1dd3dded36851e7dda2b14a21319000d
anisb23 <anisb@wix.com>
2026-03-30 03:02:54 +0300

    added debug and hooking to pvp dialogue func

--- addon/QueueNotifier/QueueNotifier.lua
+++ addon/QueueNotifier/QueueNotifier.lua
@@ -1,25 +1,36 @@
 QueueNotifierDB = QueueNotifierDB or {}
 
 local frame = CreateFrame("Frame")
-frame:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
 frame:RegisterEvent("PLAYER_LOGIN")
 
 frame:SetScript("OnEvent", function(self, event)
     if event == "PLAYER_LOGIN" then
         print("|cff00ccff[QueueNotifier]|r Loaded. Waiting for queue pop...")
-
-    elseif event == "UPDATE_BATTLEFIELD_STATUS" then
-        for i = 1, GetMaxBattlefieldID() do
-            local status, _, _, _, _, _, _, _, _, _, _, isSoloQueue = GetBattlefieldStatus(i)
-            if status == "confirm" and isSoloQueue and not QueueNotifierDB.confirming then
-                QueueNotifierDB.lastPop = time()
-                QueueNotifierDB.status = "popped"
-                QueueNotifierDB.confirming = true
-                print("|cff00ff00[QueueNotifier]|r Solo Shuffle queue popped! Notification sent.")
-            elseif status ~= "confirm" then
-                QueueNotifierDB.confirming = false
-            end
-        end
-
     end
+end)
+
+hooksecurefunc("PVPReadyDialog_Display", function(_, battlefieldId)
+    local status, mapName, teamSize, registeredMatch, suspendedQueue, queueType, gameType, role, asGroup, shortDescription, longDescription, isSoloQueue = GetBattlefieldStatus(battlefieldId)
+
+    print("|cffff9900[QueueNotifier DEBUG]|r PVPReadyDialog_Display fired!")
+    print("|cffff9900[QueueNotifier DEBUG]|r battlefieldId=" .. tostring(battlefieldId))
+    print("|cffff9900[QueueNotifier DEBUG]|r status=" .. tostring(status))
+    print("|cffff9900[QueueNotifier DEBUG]|r mapName=" .. tostring(mapName))
+    print("|cffff9900[QueueNotifier DEBUG]|r teamSize=" .. tostring(teamSize))
+    print("|cffff9900[QueueNotifier DEBUG]|r queueType=" .. tostring(queueType))
+    print("|cffff9900[QueueNotifier DEBUG]|r gameType=" .. tostring(gameType))
+    print("|cffff9900[QueueNotifier DEBUG]|r isSoloQueue=" .. tostring(isSoloQueue))
+
+    QueueNotifierDB.lastPop = time()
+    QueueNotifierDB.status = "popped"
+    QueueNotifierDB.debug = {
+        battlefieldId = battlefieldId,
+        status = status,
+        mapName = mapName,
+        teamSize = teamSize,
+        queueType = queueType,
+        gameType = gameType,
+        isSoloQueue = isSoloQueue,
+    }
+    print("|cff00ff00[QueueNotifier]|r Queue popped! Notification sent.")
 end)
